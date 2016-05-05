ffc2f90fcd7786d638a6053faf808b237e7ecf6e
Yu Chen <yc14600@bris.ac.uk>
2016-05-05 08:46:33 +0800

    correct version, last one is not original BTM.

--- BTM.py
+++ BTM.py
@@ -12,100 +12,103 @@ import os
 import math
 import random as rd
 
-def GibbsSampler(widf, docs,wd,T,ITER, lidf,lwd):
-
+def GibbsSampler(widf, docs,wd,T,ITER, lidf=[],lwd=[]):
+  
   # In[404]:
-
+  
   D = len(docs)
   V = max(wd)
+  W = len(wd)
+  
   ALPHA = 50/float(T)
   BETA = 5/float(V)
   WBETA = V*BETA
   TALPHA = T*ALPHA
-
+  
+  
   # In[406]:
-
-  wtp = np.zeros((T,V,V))
+  
+  wtp = np.zeros((T,V,V),dtype=np.float128)
   totz = np.zeros((T,1),dtype=int)
-  zd = np.zeros((D,1),dtype=int)
-
-  totprob = 0
-  maxprob = 0
-  prob = np.zeros((T,1))
+  zw = np.zeros((W,1),dtype=int)
+  zd = np.zeros((D,T),dtype=int)
+  
+  # initialise
   di = 0
   for d in docs:
+    k = rd.randint(0,T-1)
+    zd[di,k] += 1
+    totz[k] += 1
+    zw[d[0]] = k
+    
+    for i in range(d[0]+1,d[1]+1):
       k = rd.randint(0,T-1)
-      zd[di] = k
+      zd[di,k] += 1
       totz[k] +=1
-      wtp[k,wd[d[0]:d[1]]-1,wd[d[0]+1:d[1]+1]-1]+=1
-      di+=1
-
-
+      zw[i] = k
+      wtp[k,wd[i-1]-1,wd[i]-1] += widf[wd[i-1]-1,wd[i]-1]
+    
+    di+=1
+  
+  
   # In[408]:
-
-  #prob = np.zeros((T,1))
+  
+  # Gibbs Sampling
   for itr in range(ITER):
     print 'iter:',itr
     di = 0
     for d in docs:
-      u = zd[di]
-      totz[u]-=1
-
-      wtp[u,wd[d[0]:d[1]]-1,wd[d[0]+1:d[1]+1]-1]-=1
-      totprob = 0
-      prob = totz + ALPHA
+      for i in range(d[0],d[1]+1):
+          u = zw[i]
+          totz[u]-=1
+          zd[di,u]-=1
+          tmp1 = 1
+          if i > d[0]:
+            wtp[u,wd[i-1]-1,wd[i]-1] -= widf[wd[i-1]-1,wd[i]-1]
+            tmp1 = (wtp[:,wd[i-1]-1,wd[i]-1] + BETA)/(np.sum(wtp[:,wd[i-1]-1,:],1)+WBETA)
       
-      tmp = (wtp[:,wd[d[0]:d[1]]-1,wd[d[0]+1:d[1]+1]-1] + BETA)/(np.sum(wtp[:,wd[d[0]:d[1]]-1,:],min(2,d[1]-d[0]))+WBETA)
-
-      if d[1]-d[0] > 1:
-        prob = (prob.transpose() * np.prod(tmp,1))[0]
-      else:
-        prob = (prob.transpose() * tmp)[0]
-
-      totprob = sum(prob)
-      #sample from topic distribution
-      r = rd.random() * totprob
-      maxprob = prob[0]
-      k=0
-      while(r > maxprob):
-        #print k,r,maxprob
-        k+=1
-        maxprob += prob[k]
-      
-      zd[di] = k
-      wtp[k,wd[d[0]:d[1]]-1,wd[d[0]+1:d[1]+1]-1]+=1
-      totz[k]+=1
+          totprob = 0
+          prob = (zd[di,:]+ALPHA)/(d[1]-d[0]+1+TALPHA) * tmp1
+          
+          totprob = sum(prob)
+          #sample from topic distribution
+          r = rd.random() * totprob
+          maxprob = prob[0]
+          k=0
+          while(r > maxprob):
+            #print k,r,maxprob
+            k+=1
+            maxprob += prob[k]
+          
+          zw[i] = k
+          zd[di,k]+=1
+          totz[k]+=1
+          if i > d[0]:
+            wtp[k,wd[i-1]-1,wd[i]-1] += widf[wd[i-1]-1,wd[i]-1]
       di+=1
-  return(wtp,[], zd,totz)
+  return(wtp, zw, np.argmax(zd,1), totz)
 
 
 
 
 
+# In[ ]:
 
 def predictDoc(wtp,wd,docs):
 
-  #print wtp
-  #wd = rdf.word
   zd = np.zeros(len(docs))
   di = 0
   prob = np.zeros((len(docs),wtp.shape[0]))
   for d in docs:
-    tmp1 = (wtp[:,wd[d[0]:d[1]]-1,wd[d[0]+1:d[1]+1]-1])
-    if d[1]-d[0] > 1:
+    tmp1 = wtp[:,wd[d[0]:d[1]]-1,wd[d[0]+1:d[1]+1]-1]
+    if (d[1]-d[0] > 1):
       prob[di,:] += np.sum(np.log(tmp1),1)
-    else:
+    else :
       prob[di,:] += np.log(tmp1)
+               
     zd[di]=np.argmax(prob[di,:])
     di+=1
   return (zd,prob)
-
-# In[ ]:
-
-
-
-
-# In[ ]:
 
 
 
